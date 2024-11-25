8d0f95da845219f62c5b04433d2f0f258921771f
Caio Marins <caio.marins011@gmail.com>
2024-11-25 10:10:49 -0300

    feat: dark mode

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Verifica o estado preferido de modo escuro do usuário no localStorage
     if (localStorage.getItem('theme') === 'dark') {
         document.body.classList.remove('light-mode');
@@ -11,12 +11,5 @@ document.addEventListener('DOMContentLoaded', function() {
         .addEventListener('click', function () {
             document.body.classList.toggle('light-mode');
             document.body.classList.toggle('dark-mode');
-
-            // Salva a preferência no localStorage
-            if (document.body.classList.contains('dark-mode')) {
-                localStorage.setItem('theme', 'dark');
-            } else {
-                localStorage.setItem('theme', 'light');
-            }
-        });
-});
+        })
+})
